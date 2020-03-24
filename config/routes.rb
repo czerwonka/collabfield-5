d50e30ec938b9c4b1b853779d651413b5b538eef
Czerwonka <alicja.czerwonka@gmail.com>
2020-03-24 18:11:41 +0100

    Name column in users table & params in registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
-  devise_for :users
+
+  devise_for :users, :controllers => {:registrations => "registrations"}
+
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
